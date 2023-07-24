e40af7683441175fd537021475b4d3120276d63d
Abdulhamit Akaslan <hamtaksln@gmail.com>
2023-07-25 01:37:14 +0300

    Update morphTarget transition to make it smoother

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -11,174 +11,187 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import * as THREE from "three";
 
 const corresponding = {
-  A: "viseme_PP",
-  B: "viseme_kk",
-  C: "viseme_I",
-  D: "viseme_AA",
-  E: "viseme_O",
-  F: "viseme_U",
-  G: "viseme_FF",
-  H: "viseme_TH",
-  X: "viseme_PP",
+    A: "viseme_PP",
+    B: "viseme_kk",
+    C: "viseme_I",
+    D: "viseme_AA",
+    E: "viseme_O",
+    F: "viseme_U",
+    G: "viseme_FF",
+    H: "viseme_TH",
+    X: "viseme_PP",
 };
 
 export function Avatar(props) {
-  const { playAudio, script, headFollow } = useControls({
-    playAudio: false,
-    headFollow: true,
-    script: {
-      value: "welcome",
-      options: ["welcome", "pizzas"],
-    },
-  });
-
-  const audio = useMemo(() => new Audio(`/audios/${script}.mp3`), [script]);
-  const jsonFile = useLoader(THREE.FileLoader, `audios/${script}.json`);
-  const lipsync = JSON.parse(jsonFile);
-
-  useFrame(() => {
-    const currentAudioTime = audio.currentTime;
-    if (audio.paused || audio.ended) {
-      setAnimation("Idle");
-    }
-
-    Object.values(corresponding).forEach((value) => {
-      nodes.Wolf3D_Head.morphTargetInfluences[
-        nodes.Wolf3D_Head.morphTargetDictionary[value]
-      ] = 0;
-      nodes.Wolf3D_Teeth.morphTargetInfluences[
-        nodes.Wolf3D_Teeth.morphTargetDictionary[value]
-      ] = 0;
+    const { playAudio, script, headFollow } = useControls({
+        playAudio: false,
+        headFollow: true,
+        script: {
+            value: "welcome",
+            options: ["welcome", "pizzas"],
+        },
     });
 
-    for (let i = 0; i < lipsync.mouthCues.length; i++) {
-      const mouthCue = lipsync.mouthCues[i];
-      if (
-        currentAudioTime >= mouthCue.start &&
-        currentAudioTime <= mouthCue.end
-      ) {
-        nodes.Wolf3D_Head.morphTargetInfluences[
-          nodes.Wolf3D_Head.morphTargetDictionary[corresponding[mouthCue.value]]
-        ] = 1;
-        nodes.Wolf3D_Teeth.morphTargetInfluences[
-          nodes.Wolf3D_Teeth.morphTargetDictionary[
-            corresponding[mouthCue.value]
-          ]
-        ] = 1;
-        break;
-      }
-    }
-  });
+    const audio = useMemo(() => new Audio(`/audios/${script}.mp3`), [script]);
+    const jsonFile = useLoader(THREE.FileLoader, `audios/${script}.json`);
+    const lipsync = JSON.parse(jsonFile);
 
-  useEffect(() => {
-    if (playAudio) {
-      audio.play();
-      if (script === "welcome") {
-        setAnimation("Greeting");
-      } else {
-        setAnimation("Angry");
-      }
-    } else {
-      setAnimation("Idle");
-      audio.pause();
-    }
-  }, [playAudio, script]);
+    useFrame(() => {
+        const currentAudioTime = audio.currentTime;
+        if (audio.paused || audio.ended) {
+            setAnimation("Idle");
+        }
+        const meshes = [nodes.Wolf3D_Head, nodes.Wolf3D_Teeth];
+        Object.values(corresponding).forEach((value) => {
+            meshes.forEach((mesh) => {
+                const target = mesh.morphTargetDictionary[value];
+                const currentInfluence = mesh.morphTargetInfluences[target];
+                if (currentInfluence > 0) {
+                    // Decrease the influence smoothly over time
+                    mesh.morphTargetInfluences[target] = THREE.MathUtils.lerp(
+                        currentInfluence,
+                        0,
+                        0.5
+                    );
+                }
+            });
+        });
 
-  const { nodes, materials } = useGLTF("/models/646d9dcdc8a5f5bddbfac913.glb");
-  const { animations: idleAnimation } = useFBX("/animations/Idle.fbx");
-  const { animations: angryAnimation } = useFBX(
-    "/animations/Angry Gesture.fbx"
-  );
-  const { animations: greetingAnimation } = useFBX(
-    "/animations/Standing Greeting.fbx"
-  );
+        for (let i = 0; i < lipsync.mouthCues.length; i++) {
+            const mouthCue = lipsync.mouthCues[i];
+            if (
+                currentAudioTime >= mouthCue.start &&
+                currentAudioTime <= mouthCue.end
+            ) {
+                meshes.forEach((mesh) => {
+                    const target =
+                        mesh.morphTargetDictionary[
+                            corresponding[mouthCue.value]
+                        ];
+                    const currentInfluence = mesh.morphTargetInfluences[target];
+                    // Increase the influence smoothly over time
+                    mesh.morphTargetInfluences[target] = THREE.MathUtils.lerp(
+                        currentInfluence,
+                        1,
+                        0.5
+                    );
+                });
+                break;
+            }
+        }
+    });
 
-  idleAnimation[0].name = "Idle";
-  angryAnimation[0].name = "Angry";
-  greetingAnimation[0].name = "Greeting";
+    useEffect(() => {
+        if (playAudio) {
+            audio.play();
+            if (script === "welcome") {
+                setAnimation("Greeting");
+            } else {
+                setAnimation("Angry");
+            }
+        } else {
+            setAnimation("Idle");
+            audio.pause();
+        }
+    }, [playAudio, script]);
 
-  const [animation, setAnimation] = useState("Idle");
+    const { nodes, materials } = useGLTF(
+        "/models/646d9dcdc8a5f5bddbfac913.glb"
+    );
+    const { animations: idleAnimation } = useFBX("/animations/Idle.fbx");
+    const { animations: angryAnimation } = useFBX(
+        "/animations/Angry Gesture.fbx"
+    );
+    const { animations: greetingAnimation } = useFBX(
+        "/animations/Standing Greeting.fbx"
+    );
 
-  const group = useRef();
-  const { actions } = useAnimations(
-    [idleAnimation[0], angryAnimation[0], greetingAnimation[0]],
-    group
-  );
+    idleAnimation[0].name = "Idle";
+    angryAnimation[0].name = "Angry";
+    greetingAnimation[0].name = "Greeting";
 
-  useEffect(() => {
-    actions[animation].reset().fadeIn(0.5).play();
-    return () => actions[animation].fadeOut(0.5);
-  }, [animation]);
+    const [animation, setAnimation] = useState("Idle");
 
-  // CODE ADDED AFTER THE TUTORIAL (but learnt in the portfolio tutorial ♥️)
-  useFrame((state) => {
-    if (headFollow) {
-      group.current.getObjectByName("Head").lookAt(state.camera.position);
-    }
-  });
+    const group = useRef();
+    const { actions } = useAnimations(
+        [idleAnimation[0], angryAnimation[0], greetingAnimation[0]],
+        group
+    );
 
-  return (
-    <group {...props} dispose={null} ref={group}>
-      <primitive object={nodes.Hips} />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Body.geometry}
-        material={materials.Wolf3D_Body}
-        skeleton={nodes.Wolf3D_Body.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-        material={materials.Wolf3D_Outfit_Bottom}
-        skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
-        material={materials.Wolf3D_Outfit_Footwear}
-        skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Top.geometry}
-        material={materials.Wolf3D_Outfit_Top}
-        skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Hair.geometry}
-        material={materials.Wolf3D_Hair}
-        skeleton={nodes.Wolf3D_Hair.skeleton}
-      />
-      <skinnedMesh
-        name="EyeLeft"
-        geometry={nodes.EyeLeft.geometry}
-        material={materials.Wolf3D_Eye}
-        skeleton={nodes.EyeLeft.skeleton}
-        morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
-        morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
-      />
-      <skinnedMesh
-        name="EyeRight"
-        geometry={nodes.EyeRight.geometry}
-        material={materials.Wolf3D_Eye}
-        skeleton={nodes.EyeRight.skeleton}
-        morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
-        morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
-      />
-      <skinnedMesh
-        name="Wolf3D_Head"
-        geometry={nodes.Wolf3D_Head.geometry}
-        material={materials.Wolf3D_Skin}
-        skeleton={nodes.Wolf3D_Head.skeleton}
-        morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
-        morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
-      />
-      <skinnedMesh
-        name="Wolf3D_Teeth"
-        geometry={nodes.Wolf3D_Teeth.geometry}
-        material={materials.Wolf3D_Teeth}
-        skeleton={nodes.Wolf3D_Teeth.skeleton}
-        morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
-        morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
-      />
-    </group>
-  );
+    useEffect(() => {
+        actions[animation].reset().fadeIn(0.5).play();
+        return () => actions[animation].fadeOut(0.5);
+    }, [animation]);
+
+    // CODE ADDED AFTER THE TUTORIAL (but learnt in the portfolio tutorial ♥️)
+    useFrame((state) => {
+        if (headFollow) {
+            group.current.getObjectByName("Head").lookAt(state.camera.position);
+        }
+    });
+
+    return (
+        <group {...props} dispose={null} ref={group}>
+            <primitive object={nodes.Hips} />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Body.geometry}
+                material={materials.Wolf3D_Body}
+                skeleton={nodes.Wolf3D_Body.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+                material={materials.Wolf3D_Outfit_Bottom}
+                skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+                material={materials.Wolf3D_Outfit_Footwear}
+                skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Outfit_Top.geometry}
+                material={materials.Wolf3D_Outfit_Top}
+                skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Hair.geometry}
+                material={materials.Wolf3D_Hair}
+                skeleton={nodes.Wolf3D_Hair.skeleton}
+            />
+            <skinnedMesh
+                name="EyeLeft"
+                geometry={nodes.EyeLeft.geometry}
+                material={materials.Wolf3D_Eye}
+                skeleton={nodes.EyeLeft.skeleton}
+                morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
+                morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+            />
+            <skinnedMesh
+                name="EyeRight"
+                geometry={nodes.EyeRight.geometry}
+                material={materials.Wolf3D_Eye}
+                skeleton={nodes.EyeRight.skeleton}
+                morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
+                morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+            />
+            <skinnedMesh
+                name="Wolf3D_Head"
+                geometry={nodes.Wolf3D_Head.geometry}
+                material={materials.Wolf3D_Skin}
+                skeleton={nodes.Wolf3D_Head.skeleton}
+                morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+                morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+            />
+            <skinnedMesh
+                name="Wolf3D_Teeth"
+                geometry={nodes.Wolf3D_Teeth.geometry}
+                material={materials.Wolf3D_Teeth}
+                skeleton={nodes.Wolf3D_Teeth.skeleton}
+                morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+                morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+            />
+        </group>
+    );
 }
 
 useGLTF.preload("/models/646d9dcdc8a5f5bddbfac913.glb");
